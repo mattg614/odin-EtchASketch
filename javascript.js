e93cd8e6af9edef666a9b187de2c8d0debd2e906
Matthew Garza <mattg614@gmail.com>
2022-02-26 12:18:23 -0500

    Hard code a grid without JS

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-alert("hello world");
+//Etch-A-Sketch
+
+//End goal is to take users input on a prompt for number of squares by number of squares
+//Grid will be generated, when user puts mouse over the grid it will change from white to black
+
+//need to determine how to use javascript to make a grid of boxes
+//Abstract that into a function that takes user input to make different size boxes
+//Use the hover feature to change the box colors
+
+//more advanced features after this is done...
+
+
+
