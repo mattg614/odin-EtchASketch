27060016cb6362b85b3c091bcfce491d1fb40c49
Matthew Garza <mattg614@gmail.com>
2022-02-26 19:39:40 -0500

    Add random color function

--- javascript.js
+++ javascript.js
@@ -103,7 +103,8 @@ function removeChildren() {
     }
 }
 function getRandomColor() {
-    return 'red';
+    const randomColor=Math.floor(Math.random()*16777215).toString(16);
+    return '#'+randomColor;
 }
 insertGridBlocks(GRIDSIZE);
 
